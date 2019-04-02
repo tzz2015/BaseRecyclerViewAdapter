a745e093b136ffa211b9911fc7f8d7dcb0a381f8
tzz2015 <1374329135@qq.com>
2019-04-02 13:41:55 +0800

    分组

--- app/src/main/java/com/hangzhou/sz/baser/fragment/Group2Fragment.java
+++ app/src/main/java/com/hangzhou/sz/baser/fragment/Group2Fragment.java
@@ -4,7 +4,9 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,6 +57,7 @@ public class Group2Fragment extends Fragment implements ItemClickPresenter, XRec
         // 点击对应的按钮生效
         mAdapter.setItemPresenter(this, 1);
         mBinding.rvList.addItemDecoration(new XPinnedHeaderItemDecoration());
+        mBinding.rvList.addItemDecoration(new DividerItemDecoration(getContext(),DividerItemDecoration.VERTICAL));
         initData();
 
     }
